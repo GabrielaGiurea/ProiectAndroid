5c7cc7bcec9378a0780bc68c9621da1289031c92
gabriela <gabri@DESKTOP-ESUBFF0>
2020-11-15 19:41:17 +0200

    Meniu

---
+++ app/src/main/java/com/example/proiectandroid/MealsActivity.java
@@ -0,0 +1,14 @@
+package com.example.proiectandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MealsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meals);
+    }
+}
